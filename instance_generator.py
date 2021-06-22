ce0b8c9e4d43c4c1faf62dd0f9c6877733cf3a5f
Charles En-Cheng Chang <71716536+CharlesEnCheng@users.noreply.github.com>
2021-06-22 12:34:53 +0100

    Update instance_generator.py

--- instance_generator.py
+++ instance_generator.py
@@ -23,6 +23,11 @@ from config import conf
 #                                                        #
 # ====================================================== #  
 
+"""
+n, k, c, d, mode are parameters described in Experiment
+n and k are given at the entry point.
+c, d, mode are given in conf.py
+"""
 def get_instance(n, k, c, d, mode, seed = None):
 
     if seed == None: pass
@@ -30,11 +35,14 @@ def get_instance(n, k, c, d, mode, seed = None):
         np.random.seed(seed=seed)
         random.seed(seed)
         
+    # random pick k nodes for the set F denoted as f_nodes
+    # F' as left_nodes
     instance = []
     tmp_set = np.random.permutation(range(n))
     f_nodes = random.sample(range(n), k)
     left_nodes = list(set(range(n)).difference(set(f_nodes)))
 
+    # F deliver to C with random weights between [20,50]
     groups = []
     for i in range(len(f_nodes)-1):
         groups.append(tmp_set[(i)*int(np.floor(n/k)):(i+1)*int(np.floor(n/k))])
@@ -43,9 +51,9 @@ def get_instance(n, k, c, d, mode, seed = None):
     for s in range(len(f_nodes)):
         for node in groups[s]:
             instance.append([f_nodes[s], node, np.random.randint(20,50)])
-
+    
+    # F' deliver to C with random weights between [c,d] and two settings of degrees   
     for s in left_nodes:
-          
         if mode == 0:
             c_nodes = random.sample(range(n), np.random.randint(1, n))
         elif mode == 1:
@@ -68,13 +76,10 @@ if __name__ == '__main__':
         if not os.path.isdir(conf.path.data + folder): 
             os.mkdir(conf.path.data + folder)        
 
-    # list contents (n, k, c, d, mode, seed)
-    
-    # train section - n(5,9), k(2,n-1), cd=(100,400), mode = 0 
-    # train section - n(5,9), k(2,n-1), cd=(10,30),   mode = 0     
-    # train section - n(5,9), k(2,n-1), cd=(20,50),   mode = 1      
 
     for c,d,mode in [(100,400,0), (10,30,0),(20,50,1)]:
+        
+        # create training instances
         for figure in range(20000):
             np.random.seed(seed=figure)
             n = np.random.randint(7, 9)
@@ -85,23 +90,19 @@ if __name__ == '__main__':
             path = conf.path.data + 'train/' + '{0:03}_{1:03}_{2}_{3:04}_{4}_{5}.npy'.format(c,d,mode,figure,n,k)
             np.save(path, instance)
             
-    # valid & test section - n(15), k(5), cd=(100,400), mode = 0 
-    # valid & test section - n(15), k(5), cd=(10,30),   mode = 0     
-    # valid & test section - n(15), k(5), cd=(20,50),   mode = 1    
-         
+        
+        # create test & validation instances
         val_num = 100
         for figure in range(val_num):
             np.random.seed(seed=figure)
             n = 15; k = 5
             instance = get_instance(n, k, c, d, mode, seed = figure)
             instance = np.array(instance)
-            # file name c_d_mode_figure.txt
             path = conf.path.data + 'valid/' + '{0:03}_{1:03}_{2}_{3:04}_{4}_{5}.npy'.format(c,d,mode,figure,n,k)
             np.save(path, instance)
             
             instance = get_instance(n, k, c, d, mode, seed = figure+val_num*2)
             instance = np.array(instance)
-            # file name c_d_mode_figure.txt
             path = conf.path.data + 'test/' + '{0:03}_{1:03}_{2}_{3:04}_{4}_{5}_test.npy'.format(c,d,mode,figure,n,k)
             np.save(path, instance)
             
